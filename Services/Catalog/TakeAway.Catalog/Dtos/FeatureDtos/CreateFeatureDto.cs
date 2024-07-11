d6a6c6d71aa2878a20d346fd03d677a506d50d2d
Talha <99talhayildiz@gmail.com>
2024-07-11 21:00:29 +0300

    Add project files.

---
+++ Services/Catalog/TakeAway.Catalog/Dtos/FeatureDtos/CreateFeatureDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TakeAway.Catalog.Dtos.FeatureDtos
+{
+    public class CreateFeatureDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; }
+    }
+}
---
+++ Services/Catalog/TakeAway.Catalog/Dtos/SliderDtos/CreateSliderDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TakeAway.Catalog.Dtos.SliderDtos
+{
+    public class CreateSliderDto
+    {
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
