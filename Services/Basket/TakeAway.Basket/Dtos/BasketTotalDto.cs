1e1e341e8e323a9ee97a2626ff28ba46d4eaf650
Talha <99talhayildiz@gmail.com>
2024-07-31 20:34:04 +0300

    Completed the Basket Service

---
+++ Services/Basket/TakeAway.Basket/Dtos/BasketTotalDto.cs
@@ -0,0 +1,10 @@
+﻿namespace TakeAway.Basket.Dtos
+{
+    public class BasketTotalDto
+    {
+        public string UserId { get; set; }
+        public List<BasketItemDto> BasketItems { get; set; }
+        public decimal TotalPrice { get => BasketItems.Sum(x => x.Price * x.Quantity); }
+
+    }
+}
---
+++ Services/Basket/TakeAway.Basket/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.Extensions.Options;
+using TakeAway.Basket.LoginServices;
+using TakeAway.Basket.Services;
+using TakeAway.Basket.Settings;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<ILoginService, LoginService>();
+builder.Services.AddScoped<IBasketService, BasketService>();
+builder.Services.Configure<RedisSettings>(builder.Configuration.GetSection("RedisSettings"));
+builder.Services.AddScoped<RedisService>(sp =>
+{
+    var redisSettings = sp.GetRequiredService<IOptions<RedisSettings>>().Value;
+    var redis = new RedisService(redisSettings.Host, redisSettings.Port);
+    redis.Connect();
+    return redis;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
