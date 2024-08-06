f8e9bb084d7c244492780f76b29d0b93ed6c3a07
Talha <99talhayildiz@gmail.com>
2024-08-06 20:24:59 +0300

    Comment Microservices Operations

---
+++ Services/Comment/TakeAway.Comment/DAL/Entities/UserComment.cs
@@ -0,0 +1,14 @@
+﻿namespace TakeAway.Comment.DAL.Entities
+{
+    public class UserComment
+    {
+        public int UserCommandId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string UserId { get; set; }
+        public string CommentDetail { get; set; }
+        public string ProductId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool Status { get; set; }
+    }
+}
